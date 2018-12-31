88eb430c9cca890e6c4a83f94708065e5a63e426
Manish Basnet <manish.basnetnp@gmail.com>
2018-12-31 18:45:23 +0545

    initialised project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+  render() {
+    return <div>
+      <h1>Select a product and pay for it</h1>
+    </div>
+  }
+}
+
+export default App;
